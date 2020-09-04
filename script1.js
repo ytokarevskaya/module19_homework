b2bb86e5035c6eddb417540bbc170964d5fc7652
Denis <trueharis@gmail.com>
2020-09-04 20:21:43 +0300

    master

--- script1.js
+++ script1.js
@@ -1,3 +1,14 @@
-document.querySelector('.menu-icon-wrapper').onclick = function () {
-    document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
 }
